9ad2ad2b54ef89de3d1526bc9d159659d0fd8ff7
tigerisaac <isaac.cw.zhang@gmail.com>
2025-05-05 21:32:13 -0500

    Get a windows executable fo this

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const publicDir = path.join(__dirname, 'public');
+
+app.use(express.static(publicDir));
+app.get('*', (_, res) => res.sendFile(path.join(publicDir, 'index.html')));
+
+app.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
